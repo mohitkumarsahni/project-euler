b389ef16f606fb33833ab0c2a8ca48a035d24b5e
mohitkumarsahni <mohitkumarsahni@gmail.com>
2020-02-09 16:57:58 +0530

    Solved Problem 5.

---
+++ problem-5/pb-5.js
@@ -0,0 +1,19 @@
+function euclidGCD(x,y){    //y is smaller number.
+    if(y===0)
+        return x;
+    return euclidGCD(y, (x%y));
+}
+
+function lcm(x,y){
+    return ((x*y)/euclidGCD(x,y));
+}
+
+function arrLCM(n){
+    let allLCM = 1;
+    for(let i=1; i<=n; i++){
+        allLCM = lcm(i, allLCM);
+    }
+    return allLCM;
+}
+
+console.log(arrLCM(process.argv[2]));
